bad4b068357c673729b10ddaaf37073a1ad88dc4
saheed ogunsolu <saheedogunsolu2016@gmail.com>
2021-08-23 01:01:49 +0100

    hello

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ for (let i = 0; i < questions.length; i++) {
   questions[i].addEventListener("click", () => {
     questions[i].classList.toggle("active");
     let answer = questions[i].nextElementSibling;
+
     if (answer.style.display === "block") {
       answer.style.display = "none";
     } else {
